b951fecb51af3a3ecbe519b76f305d9a491497de
BenLaw859 <blawrence111098@gmail.com>
2018-04-12 19:47:39 +0100

    Added more javadoc comments and made it so you cannot try to connect without internet

--- src/XMLReader.java
+++ src/XMLReader.java
@@ -2,17 +2,14 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
+import java.net.UnknownHostException;
+
 
 public class XMLReader {
   private static String searchType;
@@ -23,18 +20,16 @@ public class XMLReader {
   /**
    * Reads the XML data for the specific query, the cache is always checked first and then if no file is found a new
    * cache file is created and that is what is used. A switch statement is used depending on the searchType to see
-   * what
-   * @param queryURL
-   * @param searchType
-   * @return
-   * @throws Exception
+   * what search type the user has input and therefore what should be outputted
+   * @param queryURL The URL formatted earlier that is used to retrieve data from the api
+   * @param searchType Either venue,author or publication to be used to find out what to return
+   * @return The output string appended to by the parsing method called
+   * @throws Exception thrown by encoding, newDocumentBuilding and parsing to doc
    */
   public static String readXML(String queryURL,String searchType) throws Exception{
     XMLReader.queryURL = queryURL;
     outputString = new StringBuilder();
-    StringBuilder result = new StringBuilder();
     File fileInCache = new File(W09Practical.getCache(), URLEncoder.encode(queryURL,"UTF-8"));
-    BufferedReader rd;
     if (!fileInCache.exists()) {
       query(queryURL);
     }
@@ -56,6 +51,10 @@ public class XMLReader {
     return outputString.toString();
   }
 
+  /**
+   * Uses the DOM parser to get the relevant information for the venue query and format it onto the output string
+   * @param doc Document containing the XML information
+   */
   private static void parseVenueXML(Document doc) {
     NodeList infoNodes = doc.getElementsByTagName("info");
       for (int i = 0; i < infoNodes.getLength(); i++) {
@@ -65,6 +64,10 @@ public class XMLReader {
 
   }
 
+  /**
+   * Uses the DOM parser to get the relevant information for the publication query and format it onto the output string
+   * @param doc Document containing the XML information
+   */
   private static void parsePublicationXML(Document doc) {
     NodeList infoNodes = doc.getElementsByTagName("info");
 
@@ -75,6 +78,11 @@ public class XMLReader {
     }
   }
 
+  /**
+   * Uses the DOM parser to get the relevant information for the author query and format it onto the output string. Another
+   * method call is made to get the information from the other url contained within doc about the author
+   * @param doc Document containing the XML information
+   */
   private static void parseAuthorXML(Document doc) {
     NodeList infoNodes = doc.getElementsByTagName("info");
     for (int i = 0; i < infoNodes.getLength(); i++) {
@@ -83,12 +91,19 @@ public class XMLReader {
       try {
         authorData = getAuthorData(eElement.getElementsByTagName("url").item(0).getTextContent()+".xml");
       } catch (Exception e) {
-        System.out.println("");
+        System.out.println("Unable to get author data");
       }
       outputString.append(eElement.getElementsByTagName("author").item(0).getTextContent()+ " - " + authorData[0] + " publications with " + authorData[1] + " co-authors.\n");
      }
   }
 
+  /**
+   * This method makes the second XML query that is necessary for the publication and co-author information to be returned
+   * regarding some authors
+   * @param authorURL The secondary URL needed to get all the information about the author
+   * @return returns the number of publications and the number of co authors in an array with two elements
+   * @throws Exception Exception due to URL encoding, and making the Document
+   */
   private static int[] getAuthorData(String authorURL) throws Exception{
     int[] outputArray = new int[2];
     File fileInCache = new File(W09Practical.getCache(), URLEncoder.encode(authorURL,"UTF-8"));
@@ -102,6 +117,8 @@ public class XMLReader {
     NodeList publicationsList = doc.getElementsByTagName("dblpperson");
     NodeList coAuthorList = doc.getElementsByTagName("coauthors");
     Node publications = publicationsList.item(0);
+    //This is necessary because if coAuthorList.item(0) is equal to 0 this causes strange problems when outputting the result
+    // i.e. it doesn't just return 0 or null
     if (coAuthorList.getLength() >= 1) {
       Node coAuthors = coAuthorList.item(0);
       Element coAuthorsElement = (Element) coAuthors;
@@ -114,10 +131,23 @@ public class XMLReader {
     return outputArray;
   }
 
+  /**
+   * This method actually gets the XML from the api and then calls necessary methods to store it in the cache. Also
+   * checks if DBLP can actually be accessed before trying to do so
+   * @param queryURL The URL that needs to be queried
+   * @throws Exception Exceptions thrown for connecting to the api and creating a URL
+   */
   private static void query(String queryURL) throws Exception{
     URL url = new URL(queryURL);
     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
     conn.setRequestMethod("GET");
+    try {
+      conn.getResponseCode();
+    } catch (UnknownHostException e) {
+      System.out.println("Unable to connect to DBLP check your internet connection");
+      System.exit(0);
+    }
     W09Practical.addFileToCache(conn,queryURL);
+
   }
 }
